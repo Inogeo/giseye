7200bdd7ee9700b52744d0c21f45e1dd0640b049
Inogeo <pierre.hardy5@gmail.com>
2022-12-14 13:47:24 +0100

    feat(toolbar): improvement of toolbar responsivity

--- app/src/App.js
+++ app/src/App.js
@@ -64,43 +64,73 @@ function App() {
         </nav> */}
 
       {/* Content */}
-      <div className='uk-grid-collapse js-filter' uk-sortable="handle: .uk-sortable-handle" style={{ height: 'calc(100% - 80px)' }} uk-grid=''>
 
-        {/*Catalog*/}
-        <Catalog handleLayerAdd={handleLayerAdd}></Catalog>
+      {/*Toolbar*/}
+      <div className='uk-grid-collapse uk-height-1-1' uk-grid=''>
 
-        {/* Main map */}
-        <div className='uk-width-expand@m uk-height-1-1 uk-text-left' filter-catalog='true' filter-map='true' filter-maponly='true'>
-          <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} className="uk-height-1-1">
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-            />
-            {mapLayersDOM}
-          </MapContainer>
-        </div>
-
-        {/* Main map legend */}
-        <div className='uk-width-1-6@l uk-width-1-5@m uk-padding-small uk-text-left uk-background-default' filter-catalog='true' filter-map='true'>
-          <p className='uk-text-large'><span className="uk-sortable-handle uk-margin-small-right uk-text-center" uk-icon="icon: table"></span>Legend</p>
-          <hr></hr>
-          <ul uk-accordion='' uk-sortable="handle: .uk-sortable-handle">
-            <li className='uk-open uk-background-muted uk-padding-small'>
-              <a className='uk-accordion-title' href="/" uk-tooltip="Drag/Drop to re-order layers; delay: 500">
-                <span className="uk-sortable-handle uk-margin-small-right uk-text-center" uk-icon="icon: table" uk-tooltip="Change layer order"></span>
-                Layer 1
+  
+        <div className='uk-width-auto uk-height-1-1 uk-text-left uk-background-primary uk-light' filter-catalog='true' filter-map='true' filter-maponly='true'>
+          <ul className="uk-iconnav uk-iconnav-vertical">
+            <li uk-filter-control="[filter-maponly='true']">
+              <a href="/" className="uk-padding-small uk-padding-remove-bottom">
+                <span uk-icon="icon: location" uk-tooltip="title: Map; pos: right;"></span>
               </a>
-              <div className='uk-accordion-content'>
-                <span className="uk-margin-small-right uk-text-center" uk-icon="icon: ban" uk-tooltip="Toggle visibility"></span>
-                <span className="uk-margin-small-right uk-text-center" uk-icon="icon: search" uk-tooltip="Zoom to extent" uk-filter-control="[filter-attable='layer']"></span>
-                <input className="uk-range" type="range" value="2" min="0" max="10" step="0.1" readOnly={true}></input>
-              </div>
+            </li>
+            <li uk-filter-control="[filter-map='true']">
+              <a href="/" className="uk-padding-small uk-padding-remove-bottom">
+                <span uk-icon="icon: list" uk-tooltip="title: Map with legend; pos: right;"></span>
+              </a>
+            </li>
+            <li className="uk-active" uk-filter-control="[filter-catalog='true']">
+              <a href="/" className="uk-padding-small uk-padding-remove-bottom">
+                <span uk-icon="icon: search" uk-tooltip="title: Add service; pos: right;"></span>
+              </a>
+            </li>
+            <li href=''>
+              <a href="https://github.com/Inogeo/giseye/issues" target="_blank" rel="noreferrer" className="uk-padding-small uk-padding-remove-bottom">
+                <span uk-icon="icon: warning" uk-tooltip="title: Report a bug; pos: right;"></span>
+              </a>
             </li>
           </ul>
         </div>
+        
+        <div className='uk-grid-collapse js-filter uk-height-1-1 uk-width-expand uk-overflow-auto' uk-sortable="handle: .uk-sortable-handle" uk-grid=''>
 
+          {/*Catalog*/}
+          <Catalog handleLayerAdd={handleLayerAdd}></Catalog>
+
+          {/* Main map */}
+          <div className='uk-width-expand@m uk-height-1-1 uk-text-left' filter-catalog='true' filter-map='true' filter-maponly='true'>
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} className="uk-height-1-1">
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+              />
+              {mapLayersDOM}
+            </MapContainer>
+          </div>
+
+          {/* Main map legend */}
+          <div className='uk-width-1-6@l uk-width-1-5@m uk-padding-small uk-text-left uk-background-default' filter-catalog='true' filter-map='true'>
+            <p className='uk-text-large'><span className="uk-sortable-handle uk-margin-small-right uk-text-center" uk-icon="icon: table"></span>Legend</p>
+            <hr></hr>
+            <ul uk-accordion='' uk-sortable="handle: .uk-sortable-handle">
+              <li className='uk-open uk-background-muted uk-padding-small'>
+                <a className='uk-accordion-title' href="/" uk-tooltip="Drag/Drop to re-order layers; delay: 500">
+                  <span className="uk-sortable-handle uk-margin-small-right uk-text-center" uk-icon="icon: table" uk-tooltip="Change layer order"></span>
+                  Layer 1
+                </a>
+                <div className='uk-accordion-content'>
+                  <span className="uk-margin-small-right uk-text-center" uk-icon="icon: ban" uk-tooltip="Toggle visibility"></span>
+                  <span className="uk-margin-small-right uk-text-center" uk-icon="icon: search" uk-tooltip="Zoom to extent" uk-filter-control="[filter-attable='layer']"></span>
+                  <input className="uk-range" type="range" value="2" min="0" max="10" step="0.1" readOnly={true}></input>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+        </div>
       </div>
-
     </div>
   );
 }
